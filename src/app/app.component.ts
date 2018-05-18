b4956da8f028c607b66d5f1242c7e378d50cf5b6
ivan <ivan.pankov.365@yandex.ru>
2018-05-19 01:56:20 +0300

    Simple marker Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  latitude: 51.678418;
-  longitude: 7.809007;
-
+  latitude: 55.70341992779656;
+  longitude: 37.53021338870849;
+  onChoseLocation(event) {
+    console.log(event);
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+  }
 }
